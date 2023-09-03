acbb9372c5481aa3de915c91e8671efa581139ac
laurentia25 <lvasilasco25@gmail.com>
2023-09-03 13:10:46 +0300

    test order submit

--- Simple_books_API_testing/tests/test_submit_order.py
+++ Simple_books_API_testing/tests/test_submit_order.py
@@ -17,7 +17,11 @@ class TestSubmitOrder(unittest.TestCase):
         - status code este 201
         - status-ul comenzii create este "true"
         """
-        pass
+        response = self.request_handler.submit_order(book_id=1, customer_name='Laurentia')
+        expected_status_code = 201
+        expected_status_order = True
+        self.assertEqual(expected_status_code, response.status_code)
+        self.assertEqual(expected_status_order, response.json()['created'])
 
     def test_order_unavailable_book(self):
         """
@@ -25,7 +29,11 @@ class TestSubmitOrder(unittest.TestCase):
         - status code este 404
         - mesajul de eroare este cel asteptat
         """
-        pass
+        response = self.request_handler.submit_order(2, 'Laurentia')
+        expected_status_code = 404
+        expected_error_msg = "This book is not in stock. Try again later."
+        self.assertEqual(expected_status_code, response.status_code)
+        self.assertEqual(expected_error_msg, response.json()['error'])
 
     def test_order_book_which_is_not_in_db(self):
         """
@@ -33,4 +41,8 @@ class TestSubmitOrder(unittest.TestCase):
         - status code este 400
         - mesajul de eroare este cel asteptat
         """
-        pass
+        response = self.request_handler.submit_order(100, 'Laurentia')
+        expected_status_code = 400
+        expected_error_msg = "Invalid or missing bookId."
+        self.assertEqual(expected_status_code, response.status_code)
+        self.assertEqual(expected_error_msg, response.json()['error'])
